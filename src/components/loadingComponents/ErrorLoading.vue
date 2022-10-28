a80c584b147c1d2f8de793f70a2b0b712c26f174
jcsantana <jcsantana@users.noreply.github.com>
2022-10-28 10:44:58 -0300

    Commit Inicial
    Começando os trabalhos

---
+++ src/components/loadingComponents/ErrorLoading.vue
@@ -0,0 +1,14 @@
+<template lang="ts">
+<div>{{ message }}</div>;
+</template>
+<script>
+export default {
+  name: 'errorLoadingComponent',
+  setup() {
+    return {
+      message: 'Ops! Algo Aconteceu',
+    };
+  },
+};
+</script>
+<style lang=""></style>
---
+++ src/components/loadingComponents/Loading.vue
@@ -0,0 +1,14 @@
+<template lang="ts">
+<div>{{ message }}</div>;
+</template>
+<script>
+export default {
+  name: 'loadingComponent',
+  setup() {
+    return {
+      message: 'Loading....',
+    };
+  },
+};
+</script>
+<style lang=""></style>
