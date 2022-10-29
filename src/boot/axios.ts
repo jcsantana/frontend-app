88c3d78f35bafbb925834f684cc837b11e3df664
jcsantana <jcsantana@users.noreply.github.com>
2022-10-29 17:34:34 -0300

    Configurando package.json
    - Configurando...

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -13,7 +13,7 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://192.168.1.2:3333/api-v1/' })
+const api = axios.create({ baseURL: 'https://jose-backend-heroku.herokuapp.com/api-v1' })
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
